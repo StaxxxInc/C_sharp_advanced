8744a7827190996e2b009d5c929892e593022ca1
Staxxx <sersersin@gmail.com>
2022-08-18 00:25:20 +0300

    25 good

--- 25/Program.cs
+++ 25/Program.cs
@@ -4,3 +4,41 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+
+
+
+
+int GetNumberA()
+{
+    Console.Write("Введите A = ");
+    int A = Convert.ToInt32(Console.ReadLine().ToString());
+    return A;
+}
+
+int GetNumberB()
+{
+    Console.Write("Введите B = ");
+    int B = Convert.ToInt32(Console.ReadLine().ToString()); 
+    return B;
+}
+
+//метод возведения в степень
+int Exponention(int A, int B)
+{
+    int result = 0;
+    result = Convert.ToInt32(Math.Pow(A, B));
+    return result;
+}
+
+//метод вывода на печать
+void Print(int result, int A, int B)
+{
+    Console.Write(A + " в степени " + B + " = " + result);
+}
+
+
+int A = GetNumberA();
+int B = GetNumberB();
+int result = Exponention(A, B);
+Print(result, A, B);
--- 27/Program.cs
+++ 27/Program.cs
@@ -3,3 +3,36 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
+string GetNumber()
+{
+	
+	Console.Write("Введите число = ");
+	number = Console.Readline();
+	// A = Convert.ToInt32(Console.Readline().ToString()); //
+	return number;	
+} 
+
+string MakeSumm(string number);
+{
+	int result = 0;
+	int a=0;
+	
+	for (int i=0; i<Lenght.number; i++)
+	{
+		a = Convert.ToInt32(number[i]);
+		result = result + a;
+	}
+	return result;
+}
+
+void Print(string number, int result)
+{
+	Console.Write("Сумма чисел "+ number + " = " + result);	
+}
+
+String Num = GetNumber();
+MakeSumm(Num);
+Print();
+

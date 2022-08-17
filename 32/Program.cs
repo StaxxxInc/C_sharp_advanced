de2d259ba1bdde7e9eaa1cc8a7a9aba1cff50b89
Staxxx <sersersin@gmail.com>
2022-08-17 22:37:59 +0300

    initial comkit

---
+++ 32/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Напишите программу замена элементов массива: 
+//  положительные элементы замените на соответствующие отрицательные, 
+//  и наоборот.
+/// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+
+//функция создания и заполнения массива данных
+int[] CreateArray(int count)
+{
+    int[] array = new int[count];              // обьявление массива
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)  // заполнение массива
+    {
+        array[i] = rand.Next(-9,10);
+    }
+
+    return array; 
+}
+
+/*int[] Change(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        // array[i] = Math.
+    }
+
+}
+*/
+
+int[] count = CreateArray(4);
+
+//Change(count);
+
+
+

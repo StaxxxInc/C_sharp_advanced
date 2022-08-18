9581d1daeaf17cde333b3e1ac9a10c428fd45801
Staxxx <sersersin@gmail.com>
2022-08-19 00:42:25 +0300

    47 rdy

--- 47/Program.cs
+++ 47/Program.cs
@@ -5,3 +5,53 @@
 //  0,5 7 -2 -0,2
 //  1 -3,3 8 -9,9
 //  8 7,8 -7,1 9
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+int GetI()
+{
+    Console.Write("Введите количество строк = ");
+    int i = Convert.ToInt32(Console.ReadLine().ToString());
+    return i;
+}
+
+int GetJ()
+{
+    Console.Write("Введите количество столбцов = ");
+    int j = Convert.ToInt32(Console.ReadLine().ToString());
+    return j;
+}
+
+int j = 0;
+j = GetJ();
+
+int i = 0; 
+i = GetI();
+
+int[,] matrix = new int[i, j];
+FillArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Сгенерированный массив = ");
+PrintArray(matrix);
+Console.WriteLine();
